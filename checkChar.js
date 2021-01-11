db0c081316333b3a48ede5592515e6dd359e76cf
HuydDo <huydinhdo@yahoo.com>
2021-01-10 21:00:27 -0700

    modified solution

--- checkChar.js
+++ checkChar.js
@@ -1,26 +1,25 @@
 // var strings = 'this iS a TeSt 523 Now!';
-var strings = '@'
-var i=0;
-var character='';
-while (i <= strings.length){
-    character = strings.charAt(i);
-    if (!isNaN(character * 1)){
-        console.log('character is numeric');
-    }else{
-        if (character == character.toUpperCase()) {
-            console.log ('upper case true');
-        }
-        if (character == character.toLowerCase()){
-            console.log ('lower case true');
-        }
-    }
-    i++;
-}
+// var i=0;
+// var character='';
+// while (i <= strings.length){
+//     character = strings.charAt(i);
+//     if (!isNaN(character * 1)){
+//         console.log('character is numeric');
+//     }else{
+//         if (character == character.toUpperCase()) {
+//             console.log ('upper case true');
+//         }
+//         if (character == character.toLowerCase()){
+//             console.log ('lower case true');
+//         }
+//     }
+//     i++;
+// }
 
 function solution(s) {
   var c = s[0];
   // var c = s.charAt(0);
-  if ((!isNaN(c * 1))){
+  if (c >= '0' && c <= '9'){
     return 'digit'
   } else if (c == c.toUpperCase()) {
     return 'upper';
@@ -31,5 +30,5 @@ function solution(s) {
   }
 }
 
-var str = solution('17')
+var str = solution('9')
 console.log(str)
