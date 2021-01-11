c8d83ac2b7cdecb4f1e3faf91e17ddcafaf7024a
HuydDo <huydinhdo@yahoo.com>
2021-01-10 20:36:45 -0700

    added counter

---
+++ counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Counter</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'counter',
+  };
+</script>
+
+<style>
+  .counter-button {
+    font-size: 1 rem;
+    padding: 5px 10px;
+    color: #585858
+  }
+</style>
