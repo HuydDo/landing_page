183d8db42bcb4bb2371198c0ffe480b462626b60
HuydDo <huydinhdo@yahoo.com>
2021-01-15 12:27:23 -0700

    added my-app folder

---
+++ my-app/src/App.vue
@@ -0,0 +1,63 @@
+<!--
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+-->
+
+
+<template>
+    <div>
+      <h2 class='counter'>{{counter}}</h2>
+      <button @click="incrementValue" class="counter-button">Click</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        counter: 42
+      }
+    },
+    methods: {
+      incrementValue() {
+        this.counter++;
+      }
+    }
+  };
+</script>
+
+<style>
+  .counter-button {
+    font-size: 1 rem;
+    padding: 5px 10px;
+    color: #585858
+  }
+</style>
+
+
